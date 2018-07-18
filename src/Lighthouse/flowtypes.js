f5fce034a676db47e399dcf9e4cb4471aa43f139
James Lefrère <james@colony.io>
2018-07-18 17:34:42 +0200

    Define Lighthouse types

---
+++ src/Lighthouse/flowtypes.js
@@ -0,0 +1,53 @@
+/* @flow */
+
+import type { ILoader, ContractData, Query } from '../interface/Loader';
+import type { IParser } from '../interface/Parser';
+import type {
+  ConstantSpecs,
+  ContractSpec,
+  EventSpecs,
+  MethodSpecs,
+  PartialConstantSpecs,
+  PartialEventSpecs,
+  PartialMethodSpecs,
+} from '../interface/ContractSpec';
+
+import { LOADER_NAME_MAP, PARSER_NAME_MAP } from './constants';
+
+export type LoaderName = $Keys<typeof LOADER_NAME_MAP>;
+
+export type LoaderSpec = {
+  name?: LoaderName,
+  options?: Object,
+};
+
+export type ParserName = $Keys<typeof PARSER_NAME_MAP>;
+
+export type ParserSpec = {
+  name?: ParserName,
+  options?: Object,
+};
+
+export type LighthouseArgs = {
+  contractData?: ContractData,
+  constants?: PartialConstantSpecs,
+  events?: PartialEventSpecs,
+  loader?: ILoader | LoaderSpec,
+  methods?: PartialMethodSpecs,
+  parser?: IParser | ParserSpec,
+  query: Query,
+};
+
+export type {
+  ConstantSpecs,
+  ContractData,
+  ContractSpec,
+  EventSpecs,
+  ILoader,
+  IParser,
+  MethodSpecs,
+  PartialConstantSpecs,
+  PartialEventSpecs,
+  PartialMethodSpecs,
+  Query,
+};
