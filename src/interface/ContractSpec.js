b1b6ee2b37d2fd8c49c871bbbd435efdae477018
James Lefrère <james@colony.io>
2018-07-18 17:20:04 +0200

    Contract Specs
    * Define `ContractSpec` and its constituent parts
    * Define partial versions of the specs (used for overrides)
    * Define `address` on `ContractSpec`

--- src/interface/ContractSpec.js
+++ src/interface/ContractSpec.js
@@ -1,6 +1,5 @@
 /* @flow */
 
-import type { ContractData } from './Loader';
 import type { ParamsSpec } from './Params';
 
 export type MethodSpec = {
@@ -21,9 +20,51 @@ export type ConstantSpec = {
   output: ParamsSpec,
 };
 
+export type EventSpecs = {
+  [eventName: string]: EventSpec,
+};
+
+export type MethodSpecs = {
+  [methodName: string]: MethodSpec,
+};
+
+export type ConstantSpecs = {
+  [constantName: string]: ConstantSpec,
+};
+
+export type PartialMethodSpec = {
+  input?: ParamsSpec,
+  isPayable?: boolean,
+  name?: string,
+  output?: ParamsSpec,
+};
+
+export type PartialEventSpec = {
+  name?: string,
+  output?: ParamsSpec,
+};
+
+export type PartialConstantSpec = {
+  name?: string,
+  input?: ParamsSpec,
+  output?: ParamsSpec,
+};
+
+export type PartialEventSpecs = {
+  [eventName: string]: PartialEventSpec,
+};
+
+export type PartialMethodSpecs = {
+  [methodName: string]: PartialMethodSpec,
+};
+
+export type PartialConstantSpecs = {
+  [constantName: string]: PartialConstantSpec,
+};
+
 export type ContractSpec = {
-  contractData: ContractData,
-  events: Array<EventSpec>,
-  methods: Array<MethodSpec>,
-  constants: Array<ConstantSpec>,
+  address: string,
+  events: EventSpecs,
+  methods: MethodSpecs,
+  constants: ConstantSpecs,
 };
