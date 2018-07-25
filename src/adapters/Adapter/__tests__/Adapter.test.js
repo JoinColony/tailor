648d8aa1623b3fa77b0417cb357976c2337525a5
James Lefrère <james@colony.io>
2018-07-25 14:18:55 +0200

    WIP: Adapter tests

---
+++ src/adapters/Adapter/__tests__/Adapter.test.js
@@ -0,0 +1,8 @@
+/* eslint-env jest */
+
+describe('Adapter', () => {
+  test('TODO', () => {
+    // TODO Adapter tests
+    expect(true).toBe(true);
+  });
+});
---
+++ src/adapters/Web3Adapter/__tests__/Web3Adapter.test.js
@@ -0,0 +1,8 @@
+/* eslint-env jest */
+
+describe('Web3Adapter', () => {
+  test('TODO', () => {
+    // TODO Web3Adapter tests
+    expect(true).toBe(true);
+  });
+});
