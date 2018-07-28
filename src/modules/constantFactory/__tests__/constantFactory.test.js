05c7c0044e706efe3eb1b68913b0c6662c897799
James Lefrère <james@colony.io>
2018-07-29 00:47:48 +0200

    Add `constantFactory`

---
+++ src/modules/constantFactory/__tests__/constantFactory.test.js
@@ -0,0 +1,177 @@
+/* eslint-env jest */
+/* eslint-disable no-underscore-dangle */
+
+import createSandbox from 'jest-sandbox';
+
+import constantFactory from '../index';
+import PARAM_TYPES from '../../paramTypes';
+
+describe('Constants', () => {
+  const sandbox = createSandbox();
+
+  const result = { z: 3 };
+
+  const mockLighthouse = {
+    adapter: {
+      call: sandbox.fn().mockResolvedValue([result.z]),
+    },
+    constantFactory,
+  };
+
+  beforeEach(() => {
+    sandbox.clear();
+  });
+
+  const outputSpec = [
+    {
+      name: 'z',
+      type: PARAM_TYPES.INTEGER,
+    },
+  ];
+  const inputs = {
+    'myConstant(uint)': [
+      {
+        name: 'a',
+        type: PARAM_TYPES.INTEGER,
+      },
+    ],
+    'myConstant(bool)': [
+      {
+        name: 'a',
+        type: PARAM_TYPES.BOOLEAN,
+      },
+    ],
+    'myConstant(uint,bool)': [
+      {
+        name: 'a',
+        type: PARAM_TYPES.INTEGER,
+      },
+      {
+        name: 'b',
+        type: PARAM_TYPES.BOOLEAN,
+      },
+    ],
+    'myConstant(uint,uint)': [
+      {
+        name: 'a',
+        type: PARAM_TYPES.INTEGER,
+      },
+      {
+        name: 'b',
+        type: PARAM_TYPES.INTEGER,
+      },
+    ],
+  };
+
+  test('Creating a constant function (output only)', async () => {
+    const spec = {
+      name: 'myConstant',
+      output: outputSpec,
+    };
+
+    const c = mockLighthouse.constantFactory(spec);
+    expect(c).toEqual(expect.any(Function));
+
+    expect(await c()).toEqual(result);
+  });
+
+  test('Creating a constant function (input and output)', async () => {
+    const spec = {
+      name: 'myConstant',
+      output: outputSpec,
+      input: {
+        'myConstant(uint)': inputs['myConstant(uint)'],
+      },
+    };
+
+    const c = mockLighthouse.constantFactory(spec);
+    expect(c).toEqual(expect.any(Function));
+
+    expect(await c(1)).toEqual(result);
+    expect(await c({ a: 1 })).toEqual(result);
+
+    let errorMessage;
+    try {
+      await c();
+    } catch (error) {
+      errorMessage = error.toString();
+    }
+
+    expect(errorMessage).toMatch('Validation for field "a" failed');
+    expect(errorMessage).not.toMatch('For overloaded function');
+  });
+
+  test('Creating a constant function (overloaded, same length)', async () => {
+    const spec = {
+      name: 'myConstant',
+      output: outputSpec,
+      input: {
+        'myConstant(uint)': inputs['myConstant(uint)'],
+        'myConstant(bool)': inputs['myConstant(bool)'],
+      },
+    };
+
+    const c = mockLighthouse.constantFactory(spec);
+    expect(c).toEqual(expect.any(Function));
+
+    expect(await c(1)).toEqual(result);
+    expect(await c(true)).toEqual(result);
+    expect(await c({ a: 1 })).toEqual(result);
+    expect(await c({ a: true })).toEqual(result);
+
+    let errorMessage;
+    try {
+      await c();
+    } catch (error) {
+      errorMessage = error.toString();
+    }
+
+    expect(errorMessage).toMatch('Errors creating overloaded function call');
+    expect(errorMessage).toMatch(
+      // eslint-disable-next-line max-len
+      'For signature "myConstant(uint)": Validation for field "a" failed: Must be a valid integer or BigNumber',
+    );
+    expect(errorMessage).toMatch(
+      // eslint-disable-next-line max-len
+      'For signature "myConstant(bool)": Validation for field "a" failed: Must be a boolean',
+    );
+  });
+
+  test('Creating a constant function (overloaded, other lengths)', async () => {
+    const spec = {
+      name: 'myConstant',
+      output: outputSpec,
+      input: {
+        'myConstant(uint)': inputs['myConstant(uint)'],
+        'myConstant(bool)': inputs['myConstant(bool)'],
+        'myConstant(uint,bool)': inputs['myConstant(uint,bool)'],
+        'myConstant(uint,uint)': inputs['myConstant(uint,uint)'],
+      },
+    };
+
+    const c = mockLighthouse.constantFactory(spec);
+    expect(c).toEqual(expect.any(Function));
+
+    expect(await c(1)).toEqual(result);
+    expect(await c(true)).toEqual(result);
+    expect(await c(1, 2)).toEqual(result);
+    expect(await c(1, false)).toEqual(result);
+    expect(await c(1, false, 1)).toEqual(result); // third param ignored
+
+    expect(await c({ a: 1 })).toEqual(result);
+    expect(await c({ a: true })).toEqual(result);
+    expect(await c({ a: 1, b: 2 })).toEqual(result);
+    expect(await c({ a: 1, b: false })).toEqual(result);
+    expect(await c({ a: 1, b: false, c: 1 })).toEqual(result); // `c` ignored
+
+    await expect(c('bad input')).rejects.toThrow(
+      'Validation for field "a" failed',
+    );
+    await expect(c('bad input', false)).rejects.toThrow(
+      'Validation for field "a" failed',
+    );
+    await expect(c(1, 'bad input')).rejects.toThrow(
+      'Validation for field "b" failed',
+    );
+  });
+});
---
+++ src/modules/constantFactory/index.js
@@ -0,0 +1,104 @@
+/* @flow */
+
+import type { FunctionCall, FunctionSignature } from '../../interface/Adapter';
+import type {
+  ConstantSpec,
+  FunctionParams,
+} from '../../interface/ContractSpec';
+import {
+  convertInput,
+  convertOutput,
+  findMatchingFunctionSignatures,
+} from '../paramConversion';
+
+/*
+ * Given function parameters, a function signature and input,
+ * simply return a function call.
+ */
+function getSingleFunctionCall(
+  functionParams: FunctionParams,
+  functionSignature: FunctionSignature,
+  ...inputParams: any
+): FunctionCall {
+  const paramsSpec = functionParams[functionSignature];
+  return {
+    args: convertInput(paramsSpec, ...inputParams),
+    functionSignature,
+  };
+}
+
+/*
+ * Given function parameters and input, find matching function signatures for
+ * those values, and attempt to get a valid function call for each signature
+ * in turn; return once a valid signature is identified.
+ * Failing that, errors for each attempted function signature are reported.
+ */
+function getOverloadedFunctionCall(
+  functionParams: FunctionParams,
+  ...inputParams: any
+): FunctionCall {
+  let args;
+  let functionSignature;
+  const errors = [];
+
+  findMatchingFunctionSignatures(functionParams, ...inputParams).forEach(
+    signature => {
+      if (args && functionSignature) return;
+
+      const paramsSpec = functionParams[signature];
+      try {
+        args = convertInput(paramsSpec, ...inputParams);
+        functionSignature = signature;
+      } catch (error) {
+        errors.push(`For signature "${signature}": ${error.message}`);
+      }
+    },
+  );
+
+  if (args && functionSignature) return { args, functionSignature };
+
+  throw new Error(
+    `Errors creating overloaded function call:\n${errors.join('\n')}`,
+  );
+}
+
+/*
+ * Given function parameters and input values, return a function call, based on
+ * either a single function signature, or finding the appropriate overloaded
+ * function signature for that input.
+ */
+function getFunctionCall(
+  functionParams: FunctionParams,
+  ...inputParams: any
+): FunctionCall {
+  const functionSignatures = Object.keys(functionParams);
+
+  return functionSignatures.length === 1
+    ? getSingleFunctionCall(
+        functionParams,
+        functionSignatures[0],
+        ...inputParams,
+      )
+    : getOverloadedFunctionCall(functionParams, ...inputParams);
+}
+
+/*
+ * Given a specification for a constant function, eeturn an async function
+ * which can be called with any valid input.
+ */
+export default function constantFactory({
+  name,
+  input = {},
+  output = [],
+}: ConstantSpec) {
+  // If input wasn't provided, use the constant name (presumed to be the
+  // function signature) to produce the function parameters we need.
+  const functionParams =
+    Object.keys(input).length === 0 ? { [name]: [] } : input;
+
+  return async (...inputParams: any) => {
+    const fnCall = getFunctionCall(functionParams, ...inputParams);
+    const callResult = await this.adapter.call(fnCall);
+    return convertOutput(output, ...callResult);
+  };
+}
